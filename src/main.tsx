2813a2fcfd3a0485ec51d9c1b0fd23b25e2af57a
lang898 <llffuture@gmail.com>
2026-08-30 20:26:56 +0700

    style: load color catalog upgrade styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import './whiterock-upgrade.css';
 import './contact-upgrade.css';
 import './product-upgrade.css';
+import './color-upgrade.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
