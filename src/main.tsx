94df00ddbcccfc9b8fdf5507562cb046f873ae40
lang898 <llffuture@gmail.com>
2026-08-30 20:25:35 +0700

    style: load product catalog upgrade styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import './whiterock-upgrade.css';
 import './contact-upgrade.css';
+import './product-upgrade.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
