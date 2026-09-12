34a7f0eae6c445a92a81cc9aa65a6483fe5ab1b6
lang898 <llffuture@gmail.com>
2026-09-12 14:03:29 +0700

    Load focused visual fixes last

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './visual-upgrade.css';
 import './ultimate-rebuild.css';
 import './detail-rebuild.css';
 import './photography-rebuild.css';
+import './visual-fixes.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
